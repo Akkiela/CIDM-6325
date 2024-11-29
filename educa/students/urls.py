4abadee9c52d3eba8c4ea8eca5e670dfe920c3d3
Akkiela <aramanathan1@buffs.wtamu.edu>
2024-11-28 23:44:33 -0600

    final_project

--- educa/students/urls.py
+++ educa/students/urls.py
@@ -29,7 +29,12 @@ urlpatterns = [
         cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
         name='student_course_detail_module',
     ),
-     path(
+    path(
+        '<pk>/module/',
+        views.CourseModuleUpdateView.as_view(),
+        name='course_module_update',
+    ),
+    path(
         'module/<int:module_id>/content/<model_name>/create/',
         views.ContentCreateUpdateView.as_view(),
         name='module_content_create',
@@ -38,5 +43,30 @@ urlpatterns = [
         'module/<int:module_id>/content/<model_name>/<id>/',
         views.ContentCreateUpdateView.as_view(),
         name='module_content_update',
+    ),
+    path(
+        'content/<int:id>/delete/',
+        views.ContentDeleteView.as_view(),
+        name='module_content_delete',
+    ),
+    path(
+        'module/<int:module_id>/',
+        views.ModuleContentListView.as_view(),
+        name='module_content_list',
+    ),
+    path(
+        'module/order/',
+        views.ModuleOrderView.as_view(),
+        name='module_order',
+    ),
+    path(
+        'content/order/',
+        views.ContentOrderView.as_view(),
+        name='content_order',
+    ),
+    path(
+        '<pk>/module/',
+        views.CourseModuleUpdateView.as_view(),
+        name='course_module_update',
     ),
 ]
